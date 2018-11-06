8462f80a6285c553316d35cf21fadd34e13c76f3
Duree Lee <2.duree.lee@gmail.com>
2018-11-06 21:28:32 +1100

    Add comments at 11. Dissect Hello Rectangle

--- course/01_hello_rectangle/solution_01_hello_rectangle/lib/main.dart
+++ course/01_hello_rectangle/solution_01_hello_rectangle/lib/main.dart
@@ -4,6 +4,62 @@
 
 import 'package:flutter/material.dart';
 
+// Dart Auto Formatter
+// menu > Code > Reformat Code with dartfmt
+
+// container widget 함수화
+/* fixed
+Widget helloRectangle() {
+  return Container(
+      color: Colors.greenAccent,
+  );
+}
+*/
+
+// stateless widget
+class HelloRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(//Container( // alt+enter while cursors inside that container you want to wrap
+      child: Container(
+        color: Colors.greenAccent,
+        height: 400.0,
+        width: 300.0,
+        child: Center( // The center widget above the container only applies to its immediate child
+          child: Text(
+            'Hello!',
+            style: TextStyle(fontSize: 40.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  //print('Hello!'); // 디버깅 콘솔에서 확인용
+
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Rectangle'),
+        ),
+        /*
+        body: Container(
+        color: Colors.greenAccent,
+        */
+        // body: helloRectangle(),
+        body: HelloRectangle(),
+      ), // 위젯의 속성 다음에 반드시 쉼표; 서식 지정에 도움
+    ),
+  );
+}
+
+/* 처음
+import 'package:flutter/material.dart';
+
 void main() {
   runApp(
     MaterialApp(
@@ -38,3 +94,4 @@ class HelloRectangle extends StatelessWidget {
     );
   }
 }
+*/
